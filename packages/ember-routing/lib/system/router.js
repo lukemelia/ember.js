87a1853ac5fa43fe47b9a916f6bb92c7b712564e
Stefan Penner <stefan.penner@gmail.com>
2013-05-18 21:19:49 -0400

    App learns to LOG_ACTIVE_GENERATION
    logs when controllers/routes are generated.

--- packages/ember-routing/lib/system/router.js
+++ packages/ember-routing/lib/system/router.js
@@ -178,6 +178,10 @@ function getHandlerFunction(router) {
 
       container.register(routeName, DefaultRoute.extend());
       handler = container.lookup(routeName);
+
+      if (get(router, 'namespace.LOG_ACTIVE_GENERATION')) {
+        Ember.Logger.info("generated -> " + routeName, { fullName: routeName });
+      }
     }
 
     if (name === 'application') {
